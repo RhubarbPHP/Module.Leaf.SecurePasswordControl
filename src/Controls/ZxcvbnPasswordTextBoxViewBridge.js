ee6b5a2a0274731a3a293bf913faffebe17ffc0d
Michael Miscampbell <mmiscampbell@gcdtech.com>
2017-11-06 09:44:46 +0000

    Fix for function formatting

--- src/Controls/ZxcvbnPasswordTextBoxViewBridge.js
+++ src/Controls/ZxcvbnPasswordTextBoxViewBridge.js
@@ -18,62 +18,60 @@ bridge.prototype.getStandardValidator = function (targetElement) {
 };
 
 bridge.prototype.isSecurePassword = function (value, successCallback, failureCallback) {
-    // return function (value, successCallback, failureCallback) {
-        var validationErrors = [];
-        if (this.model.minimumLength > 0) {
-            if (value.length < this.model.minimumLength) {
-                validationErrors.push(this.model.MINIMUM_LENGTH_KEY);
-            }
+    var validationErrors = [];
+    if (this.model.minimumLength > 0) {
+        if (value.length < this.model.minimumLength) {
+            validationErrors.push(this.model.MINIMUM_LENGTH_KEY);
         }
+    }
 
-        if (this.model.maximumLength > 0) {
-            if (value.length > this.model.maximumLength) {
-                validationErrors.push(this.model.MAXIMUM_LENGTH_KEY);
-            }
+    if (this.model.maximumLength > 0) {
+        if (value.length > this.model.maximumLength) {
+            validationErrors.push(this.model.MAXIMUM_LENGTH_KEY);
         }
+    }
 
-        if (this.model.minimumRequiredNumbers > 0) {
-            var matches = this.getRegexMatches(/[0-9]/g, value);
+    if (this.model.minimumRequiredNumbers > 0) {
+        var matches = this.getRegexMatches(/[0-9]/g, value);
 
-            if (matches.length < this.model.minimumRequiredNumbers) {
-                validationErrors.push(this.model.MINIMUM_REQUIRED_NUMBERS_KEY);
-            }
+        if (matches.length < this.model.minimumRequiredNumbers) {
+            validationErrors.push(this.model.MINIMUM_REQUIRED_NUMBERS_KEY);
         }
+    }
 
-        if (this.model.minimumRequiredUppercaseLetters > 0) {
-            var matches = this.getRegexMatches(/[A-Z]/g, value);
-            if (matches.length < this.model.minimumRequiredUppercaseLetters) {
-                validationErrors.push(this.model.MINIMUM_REQUIRED_UPPERCASE_LETTERS_KEY);
-            }
+    if (this.model.minimumRequiredUppercaseLetters > 0) {
+        var matches = this.getRegexMatches(/[A-Z]/g, value);
+        if (matches.length < this.model.minimumRequiredUppercaseLetters) {
+            validationErrors.push(this.model.MINIMUM_REQUIRED_UPPERCASE_LETTERS_KEY);
         }
+    }
 
-        if (this.model.minimumRequiredLowercaseLetters > 0) {
-            var matches = this.getRegexMatches(/[a-z]/g, value);
-            if (matches.length < this.model.minimumRequiredLowercaseLetters) {
-                validationErrors.push(this.model.MINIMUM_REQUIRED_LOWERCASE_LETTERS_KEY);
-            }
+    if (this.model.minimumRequiredLowercaseLetters > 0) {
+        var matches = this.getRegexMatches(/[a-z]/g, value);
+        if (matches.length < this.model.minimumRequiredLowercaseLetters) {
+            validationErrors.push(this.model.MINIMUM_REQUIRED_LOWERCASE_LETTERS_KEY);
         }
+    }
 
-        if (this.model.minimumRequiredSpecialCharacters > 0) {
-            var matches = this.getRegexMatches(/[(!@#$%^&*).]/g, value);
-            if (matches.length < this.model.minimumRequiredSpecialCharacters) {
-                validationErrors.push(this.model.MINIMUM_REQUIRED_SPECIAL_CHARACTERS_LETTERS_KEY);
-            }
+    if (this.model.minimumRequiredSpecialCharacters > 0) {
+        var matches = this.getRegexMatches(/[(!@#$%^&*).]/g, value);
+        if (matches.length < this.model.minimumRequiredSpecialCharacters) {
+            validationErrors.push(this.model.MINIMUM_REQUIRED_SPECIAL_CHARACTERS_LETTERS_KEY);
         }
+    }
 
-        if (this.model.minimumZxcvbnOverallScore > 0) {
-            var result = zxcvbn(value);
-            if (result.score < this.model.minimumZxcvbnOverallScore) {
-                validationErrors.push(this.model.MINIMUM_ZXCVBN_OVERALL_SCORE_KEY);
-            }
+    if (this.model.minimumZxcvbnOverallScore > 0) {
+        var result = zxcvbn(value);
+        if (result.score < this.model.minimumZxcvbnOverallScore) {
+            validationErrors.push(this.model.MINIMUM_ZXCVBN_OVERALL_SCORE_KEY);
         }
+    }
 
-        if (validationErrors.length > 0) {
-            failureCallback(validationErrors);
-        } else {
-            successCallback();
-        }
-    // }
+    if (validationErrors.length > 0) {
+        failureCallback(validationErrors);
+    } else {
+        successCallback();
+    }
 };
 
 bridge.prototype.getRegexMatches = function (regex, valueToCheck) {
